d4ea856d9d8a08416f2a1d2af9ec4dcb5b4f517d
vivianassis <vivianassis11@gmail.com>
2021-08-02 20:17:15 -0300

    Organizando informações

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-//MDB
-    <script
-        type="text/javascript"
-        src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"
-    ></script>
-
 //Carrousel
 const myCarousel = document.querySelector('#myCarousel')
 const carousel = new mdb.Carousel(myCarousel)
+
+//MDB
+        type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"
+    
+
+
